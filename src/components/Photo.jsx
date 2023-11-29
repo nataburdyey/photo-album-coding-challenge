de7b3931281794f3aedfaea6b15de2f6a72076cf
Natalia Burdyey <nata.burdey@gmail.com>
2023-11-29 08:36:26 -0500

    update readme

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -13,7 +13,7 @@ const Photo = ({ photo, added, className, canDelete, onDelete }) => {
 
     return (
         <div className='relative print:w-max print:p-10'>
-            <img key={photo.id} src={photo.url} alt={photo.title} className={`${className} print:w-max`} ref={drag} style={{ opacity }} />
+            <img key={photo.id} src={photo.url} alt={photo.title} className={className} ref={drag} style={{ opacity }} />
             {canDelete && <button onClick={() => onDelete(photo.id)} className='print:hidden absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-1 -right-1 dark:border-gray-900'>X</button>}
         </div>
     );
